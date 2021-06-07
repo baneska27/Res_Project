65928d662f8c80a8539cb007f5910ba22dff048c
Branislav Martinovic <martinovic.pr118.2017@uns.ac.rs>
2021-06-07 23:44:47 +0200

    Reader

---
+++ Reader/Helper/ReaderHelper.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader.Helper
+{
+    public class ReaderHelper
+    {
+        public void IspisiPoDataSetu(int izborIspisa2)
+        {
+            string path = "";
+            switch (izborIspisa2)
+            {
+                case 1:
+                    path = "../../../Historical/Baza/CodeAnalogCodeDigitalTable.txt";
+                    break;
+                case 2:
+                    path = "../../../Historical/Baza/CodeCustomCodeLimitsetTable.txt";
+                    break;
+                case 3:
+                    path = "../../../Historical/Baza/CodeSinglenodeCodeMultiplenodeTable.txt";
+                    break;
+                case 4:
+                    path = "../../../Historical/Baza/CodeConsumerCodeSourceTable.txt";
+                    break;
+                case 5:
+                    path = "../../../Historical/Baza/CodeMotionCodeSensorTable.txt";
+                    break;
+            }
+            FileStream stream = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            string line = "";
+            string type = "";
+            string value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                }
+
+            }
+            sr.Close();
+
+        }
+
+        public void IspisPoCode(int izborIspisa2)
+        {
+            string path = "";
+            switch (izborIspisa2)
+            {
+                case 1:
+                    path = "../../../Historical/Baza/CodeAnalogCodeDigitalTable.txt";
+                    break;
+                case 2:
+                    path = "../../../Historical/Baza/CodeAnalogCodeDigitalTable.txt";
+                    break;
+                case 3:
+                    path = "../../../Historical/Baza/CodeCustomCodeLimitsetTable.txt";
+                    break;
+                case 4:
+                    path = "../../../Historical/Baza/CodeCustomCodeLimitsetTable.txt";
+                    break;
+                case 5:
+                    path = "../../../Historical/Baza/CodeSinglenodeCodeMultiplenodeTable.txt";
+                    break;
+                case 6:
+                    path = "../../../Historical/Baza/CodeSinglenodeCodeMultiplenodeTable.txt";
+                    break;
+                case 7:
+                    path = "../../../Historical/Baza/CodeConsumerCodeSourceTable.txt";
+                    break;
+                case 8:
+                    path = "../../../Historical/Baza/CodeConsumerCodeSourceTable.txt";
+                    break;
+                case 9:
+                    path = "../../../Historical/Baza/CodeMotionCodeSensorTable.txt";
+                    break;
+                case 10:
+                    path = "../../../Historical/Baza/CodeMotionCodeSensorTable.txt";
+                    break;
+
+
+            }
+            FileStream stream = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            string line = "";
+            string type = "";
+            string value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    if (izborIspisa2 == 1)
+                    {
+                        if (data[0].Equals("CODE_ANALOG"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                        }
+                    }
+                    else if (izborIspisa2 == 2)
+                    {
+                        if (data[0].Equals("CODE_DIGITAL"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 3)
+                    {
+                        if (data[0].Equals("CODE_CUSTOM"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 4)
+                    {
+                        if (data[0].Equals("CODE_LIMITSET"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 5)
+                    {
+                        if (data[0].Equals("CODE_SINGLENODE"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 6)
+                    {
+                        if (data[0].Equals("CODE_MULTIPLENODE"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 7)
+                    {
+                        if (data[0].Equals("CODE_CONSUMER"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 8)
+                    {
+                        if (data[0].Equals("CODE_SOURCE"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 9)
+                    {
+                        if (data[0].Equals("CODE_MOTION"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+                    else if (izborIspisa2 == 10)
+                    {
+                        if (data[0].Equals("CODE_SENSOR"))
+                        {
+                            Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+                        }
+                    }
+
+                }
+
+            }
+            sr.Close();
+
+        }
+
+        public void IspisPoDatumu(DateTime from, DateTime to)
+        {
+            string path1 = "../../../Historical/Baza/CodeAnalogCodeDigitalTable.txt";
+            string path2 = "../../../Historical/Baza/CodeCustomCodeLimitsetTable.txt";
+            string path3 = "../../../Historical/Baza/CodeSinglenodeCodeMultiplenodeTable.txt";
+            string path4 = "../../../Historical/Baza/CodeConsumerCodeSourceTable.txt";
+            string path5 = "../../../Historical/Baza/CodeMotionCodeSensorTable.txt";
+
+            FileStream stream = new FileStream(path1, FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            string line = "";
+            string type = "";
+            string value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    DateTime dateInFile = DateTime.Parse(data[2]);
+                    if (dateInFile.CompareTo(from)>0 && dateInFile.CompareTo(to)<0)
+                    {
+                        Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                    }
+
+
+
+
+
+                }
+            }
+            sr.Close();
+
+            stream = new FileStream(path2, FileMode.Open);
+            sr = new StreamReader(stream);
+            line = "";
+            type = "";
+            value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    DateTime dateInFile = DateTime.Parse(data[2]);
+                    if (dateInFile.CompareTo(from) > 0 && dateInFile.CompareTo(to) < 0)
+                    {
+                        Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                    }
+
+
+
+
+
+                }
+            }
+            sr.Close();
+            stream = new FileStream(path3, FileMode.Open);
+            sr = new StreamReader(stream);
+            line = "";
+            type = "";
+            value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    DateTime dateInFile = DateTime.Parse(data[2]);
+                    if (dateInFile.CompareTo(from) > 0 && dateInFile.CompareTo(to) < 0)
+                    {
+                        Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                    }
+
+
+
+
+
+                }
+            }
+            sr.Close();
+            stream = new FileStream(path4, FileMode.Open);
+            sr = new StreamReader(stream);
+            line = "";
+            type = "";
+            value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    DateTime dateInFile = DateTime.Parse(data[2]);
+                    if (dateInFile.CompareTo(from) > 0 && dateInFile.CompareTo(to) < 0)
+                    {
+                        Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                    }
+
+
+
+
+
+                }
+            }
+            sr.Close();
+            stream = new FileStream(path5, FileMode.Open);
+            sr = new StreamReader(stream);
+            line = "";
+            type = "";
+            value = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                if (data.Length == 3)
+                {
+                    DateTime dateInFile = DateTime.Parse(data[2]);
+                    int vrednost = dateInFile.CompareTo(from);
+                    if (dateInFile.CompareTo(from) > 0 && dateInFile.CompareTo(to) < 0)
+                    {
+                        Console.WriteLine(data[0] + " " + data[1] + " " + data[2]);
+
+                    }
+
+
+
+
+
+                }
+            }
+            sr.Close();
+        }
+    }
+}
--- Reader/Program.cs
+++ Reader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Reader.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Reader
 {
     class Program
     {
+        static ReaderHelper readerHelper = new ReaderHelper();
         static void ConnectToHistorical()
         {
             string address = "net.tcp://localhost:4003/IReader";
@@ -24,7 +26,69 @@ namespace Reader
 
             binding.SendTimeout = new TimeSpan(1, 0, 0);
 
+            int izborIspisa = 0;
+            int izborIspisa2 = 0;
             string message = "";
+            while(true)
+            {
+                do
+                {
+                    Console.WriteLine("1. Ispis po DataSetu\n");
+                    Console.WriteLine("2. Ispis po Code-u\n");
+                    Console.WriteLine("3. Ispis po datumu\n");
+                    izborIspisa = Int32.Parse(Console.ReadLine());
+                } while (izborIspisa > 4);
+                
+                if(izborIspisa==1)
+                {
+                    do
+                    {
+                        Console.WriteLine("Izaberite DataSet\n");
+                        Console.WriteLine("1. Dataset 1 (CodeAnalog+CodeDigital)\n");
+                        Console.WriteLine("2. Dataset 2 (CodeCustom+CodeLimitset)\n");
+                        Console.WriteLine("3. Dataset 3 (CodeSingleNode+CodeMultipleNode\n");
+                        Console.WriteLine("4. Dataset 4 (CodeConsumer+CodeSource)\n");
+                        Console.WriteLine("5. Dataset 5 (CodeMotion+CodeSensor)\n");
+                        izborIspisa2 = Int32.Parse(Console.ReadLine());
+
+                    } while (izborIspisa2 > 6  || izborIspisa2 < 0);
+
+                    readerHelper.IspisiPoDataSetu(izborIspisa2);
+                }
+                else if(izborIspisa==2)
+                {
+                    do
+                    {
+                        Console.WriteLine("Izaberite Code\n");
+                        Console.WriteLine("1. Code Analog\n");
+                        Console.WriteLine("2. Code Digital\n");
+                        Console.WriteLine("3. Code Custom\n");
+                        Console.WriteLine("4. Code Limitset\n");
+                        Console.WriteLine("5. Code Singlenode\n");
+                        Console.WriteLine("6. Code Multiplenode\n");
+                        Console.WriteLine("7. Code Consumer\n");
+                        Console.WriteLine("8. Code Source\n");
+                        Console.WriteLine("9. Code Motion\n");
+                        Console.WriteLine("10. Code Sensor\n");
+                        izborIspisa2 = Int32.Parse(Console.ReadLine());
+
+                    } while (izborIspisa2 > 10 || izborIspisa2<0);
+
+                    readerHelper.IspisPoCode(izborIspisa2);
+                }
+                else if(izborIspisa==3)
+                {
+                    DateTime from;
+                    DateTime to;
+                    Console.WriteLine("Unos datuma OD - DO\n");
+                    Console.WriteLine("[OD] Unesite datum u formatu DD/MM/YYYY: ");
+                    from = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("[DO] Unesite datum u formatu DD/MM/YYYY: ");
+                    to = DateTime.Parse(Console.ReadLine());
+
+                    readerHelper.IspisPoDatumu(from, to);
+                }
+            }
         }
 
         static void Main(string[] args)
@@ -32,7 +96,7 @@ namespace Reader
 
             Thread t1 = new Thread(new ThreadStart(ConnectToHistorical));
             t1.Start();
-            for (; ; );
+            
         }
     }
 }
